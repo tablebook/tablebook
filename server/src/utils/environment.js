c65a60ce1fab0007bf52b56bfdf999eaa9d764e6
tuukkaviitanen <tuukka.viitanen@gmail.com>
2024-02-22 17:54:49 +0200

    added environment.js file for parsing ENV variables in server
    ensures that nodeEnv is set and checks that at least the db connectionstring for the current run mode is set. exports nodeEnv and dbConnectionstring based on nodeEnv

---
+++ server/src/utils/environment.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-console */
+import "dotenv/config";
+
+const nodeEnv = process.env.NODE_ENV;
+
+const mongodbProdConnectionstring = process.env.MONGODB_CONNECTION_STRING;
+
+const mongodbDevConnectionstring = process.env.MONGODB_DEV_CONNECTION_STRING;
+
+// Errorhandling
+
+if (!nodeEnv) {
+  console.error("NODE_ENV not set. Aborting");
+  process.exit();
+}
+
+if (nodeEnv === "production" && !mongodbProdConnectionstring) {
+  console.error(
+    "NODE_ENV set to production, but MONGODB_CONNECTION_STRING is not set. Aborting.",
+  );
+  process.exit();
+}
+
+if (nodeEnv !== "production" && !mongodbDevConnectionstring) {
+  console.error(
+    "NODE_ENV set to development/test, but MONGODB_DEV_CONNECTION_STRING is not set. Aborting.",
+  );
+  process.exit();
+}
+
+// Decide db connectionstring based on node env
+
+const dbConnectionstring =
+  nodeEnv === "production"
+    ? mongodbProdConnectionstring
+    : mongodbDevConnectionstring;
+
+const environment = {
+  nodeEnv,
+  dbConnectionstring,
+};
+
+export default environment;
