92e965c03e766c34a6a2595c19509b8e4e640bad
tuukkaviitanen <tuukka.viitanen@gmail.com>
2024-03-04 20:11:22 +0200

    Added express app listen to index.js
    After mongoose connection is created, express server starts listening for requests.

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
 import environment from "./utils/environment.js";
+import app from "./app.js";
 
 const main = async () => {
   await mongoose.connect(environment.dbConnectionstring);
+
+  app.listen(environment.port, () => {
+    console.log(`express server listeing for port ${environment.port}`);
+  });
 };
 
 main().catch((error) => console.error(error));
