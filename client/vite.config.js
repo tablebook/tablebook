82c2a9dc18d64c2249b5261ead65b5d39c2f6076
Arto-Laitinen <arto.e.laitinen@tuni.fi>
2024-03-09 19:15:25 +0200

    added esbuild and optimizeDeps to vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -12,4 +12,25 @@ export default defineConfig({
       reporter: ["text", "json", "html"],
     },
   },
+  esbuild: {
+    loader: "jsx",
+    include: /(src)\/.*\.jsx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: { ".js": "jsx" },
+      plugins: [
+        {
+          name: "load-js-files-as-jsx",
+          setup(build) {
+            build.onLoad({ filter: /(src)\/.*\.js$/ }, async (args) => ({
+              loader: "jsx",
+              contents: await fs.readFile(args.path, "utf8"),
+            }));
+          },
+        },
+      ],
+    },
+  },
 });
