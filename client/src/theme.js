de67b811d358e6359bd13e6c2a9aa5e3ffcb6a85
TyostoKarry <samuritala@hotmail.com>
2024-03-14 18:37:50 +0200

    created own theme file
    - includes all previous color palettes
    - is now exported by itself without running ReactDOM.createRoot
    - can be used in tests

---
+++ client/src/theme.js
@@ -0,0 +1,32 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#7E83A0",
+            contrastText: "#ffffff",
+        },
+        secondary: {
+            main: "#F3C297",
+            contrastText: "#000000",
+        },
+        background: {
+            main: "#FFE3BE",
+            contrastText: "#000000",
+        },
+        delete: {
+            main: "#F53C3C",
+            contrastText: "#000000",
+        },
+        confirm: {
+            main: "#00FF1A",
+            contrastText: "#000000",
+        },
+        paper: {
+            main: "#ffffff",
+            contrastText: "#000000",
+        },
+    },
+});
+
+export default theme;
