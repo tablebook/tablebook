57f563f242f9454a9e06acf7d10bdade139f326d
Tuukka Viitanen <tuukka.viitanen@gmail.com>
2024-03-17 15:04:42 +0200

    Server now serves the frontend if it's built
    Request paths starting with /api/ are reserved for the API, and other paths return the client files.

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 import "express-async-errors";
 import express from "express";
 import helmet from "helmet";
+import path from "path";
 
 import minutesController from "./controllers/minutesController.js";
 import errorHandler from "./utils/errorHandler.js";
@@ -14,5 +15,15 @@ app.use(express.json());
 app.use("/api/minutes", minutesController);
 
 app.use(errorHandler);
+
+app.get("/api/*", (req, res) => {
+  res.status(404).json({ error: "API route not found" });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "..", "..", "client", "dist", "index.html"),
+  );
+});
 
 export default app;
