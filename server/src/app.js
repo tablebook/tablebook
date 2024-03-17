b54535634275b72746688eff2782516b1b013db9
Tuukka Viitanen <tuukka.viitanen@gmail.com>
2024-03-17 15:30:09 +0200

    Added error handling for serving client files
    This could later be replaced with a static html error page

--- server/src/app.js
+++ server/src/app.js
@@ -2,9 +2,19 @@ import "express-async-errors";
 import express from "express";
 import helmet from "helmet";
 import path from "path";
+import fs from "fs";
 
 import minutesController from "./controllers/minutesController.js";
 import errorHandler from "./utils/errorHandler.js";
+
+const clientIndexPath = path.join(
+  __dirname,
+  "..",
+  "..",
+  "client",
+  "dist",
+  "index.html",
+);
 
 const app = express();
 
@@ -17,13 +27,15 @@ app.use("/api/minutes", minutesController);
 app.use(errorHandler);
 
 app.get("/api/*", (req, res) => {
-  res.status(404).json({ error: "API route not found" });
+  return res.status(404).json({ error: "API route not found" });
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "..", "..", "client", "dist", "index.html"),
-  );
+  if (!fs.existsSync(clientIndexPath)) {
+    console.error(`Client not found in "${clientIndexPath}"`);
+    return res.json({ error: "Client files not found" });
+  }
+  return res.sendFile(clientIndexPath);
 });
 
 export default app;
