2a69d92e59b27a1922e3c20656a4b3a636e7b2f0
TyostoKarry <samuritala@hotmail.com>
2024-03-17 13:38:11 +0200

    created tests for footer component

---
+++ client/src/components/Footer.test.jsx
@@ -0,0 +1,32 @@
+import { expect, test, describe, beforeEach } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { ThemeProvider } from "@mui/material/styles";
+import Footer from "./Footer.jsx";
+
+import theme from "../theme";
+
+describe("Footer", () => {
+  beforeEach(() => {
+    render(
+      <ThemeProvider theme={theme}>
+        <Footer />
+      </ThemeProvider>,
+    );
+  });
+
+  test("renders report button", () => {
+    const reportButton = screen.getByText("Report", { selector: "h5" });
+    expect(reportButton).toBeDefined();
+  });
+
+  test("renders github link", () => {
+    const githubButton = screen.getByText("GitHub", { selector: "a" });
+    expect(githubButton).toBeDefined();
+    expect(githubButton.href).toEqual("https://github.com/tablebook/tablebook");
+  });
+
+  test("renders copyright button", () => {
+    const copyrightButton = screen.getByText("Copyright", { selector: "h5" });
+    expect(copyrightButton).toBeDefined();
+  });
+});
