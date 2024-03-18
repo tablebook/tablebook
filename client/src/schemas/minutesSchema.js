72be670b1dd0ed6009a3c56b97dd4ff9af37c5a4
Tuukka Viitanen <tuukka.viitanen@gmail.com>
2024-03-18 12:40:30 +0200

    Created validation schemas on the client
    minutesSchema is almost identical to the minutesSchema on the server, but client also includes the minutes id.
    responseSchemas include CRUD response schemas that match the API documentation.

---
+++ client/src/schemas/minutesSchema.js
@@ -0,0 +1,32 @@
+import { z } from "zod";
+
+const mongoIdSchema = z
+  .string()
+  .refine((value) => /^[0-9a-fA-F]{24}$/.test(value), {
+    message: "Invalid MongoDB id",
+  });
+
+const segmentSchema = z.object({
+  name: z.string(),
+  content: z.string(),
+});
+
+const signatureSchema = z.object({
+  signer: z.string(),
+  timestamp: z.string().datetime(),
+  image: z.string(),
+});
+
+const minutesSchema = z.object({
+  name: z.string(),
+  colors: z.object({
+    primary: z.string(),
+    secondary: z.string(),
+  }),
+  segments: z.array(segmentSchema),
+  startTime: z.string().datetime(),
+  signatures: z.array(signatureSchema),
+  id: mongoIdSchema,
+});
+
+export default minutesSchema;
