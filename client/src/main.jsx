05d01d20b3b2751898552639067a0752f64ceb67
Arto-Laitinen <arto.e.laitinen@tuni.fi>
2024-03-21 17:20:59 +0200

    Wrapped everything in main.jsx with MinutesContextProvider so the context is available everywhere

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
 import App from "./App.jsx";
+import { MinutesContextProvider } from "./contexts/MinutesContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ThemeProvider theme={theme}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ThemeProvider>,
+  <MinutesContextProvider>
+    <ThemeProvider theme={theme}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ThemeProvider>
+  </MinutesContextProvider>,
 );
