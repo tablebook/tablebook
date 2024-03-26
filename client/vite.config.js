bbb9c236e45028e31296c53f563716368bd124bb
Tuukka Viitanen <tuukka.viitanen@gmail.com>
2024-03-26 22:34:43 +0200

    Docker compose dev now runs in docker internal network
    Vite proxy needs to come from env variables as it's going to be different in the docker network. Vite --host is needed to access the port from host machine network.
    Disabled mongodb logging to console. Removed obsolete version tag.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,28 +1,32 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/test/setup.js",
-    coverage: {
-      reporter: ["text", "json", "html"],
-    },
-  },
-  esbuild: {
-    loader: "jsx",
-    include: /(src)\/.*\.jsx?$/,
-    exclude: [],
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8080",
-        changeOrigin: true,
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./src/test/setup.js",
+      coverage: {
+        reporter: ["text", "json", "html"],
       },
     },
-  },
-});
+    esbuild: {
+      loader: "jsx",
+      include: /(src)\/.*\.jsx?$/,
+      exclude: [],
+    },
+    server: {
+      proxy: {
+        "/api": {
+          target: process.env.VITE_PROXY_TARGET,
+          changeOrigin: true,
+        },
+      },
+    },
+  });
+};
