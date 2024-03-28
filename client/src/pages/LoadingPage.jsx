d349dd70cea30f0853b8fe481b362006e9428203
Tuukka Viitanen <tuukka.viitanen@gmail.com>
2024-03-29 00:44:37 +0200

    Added styled loading page with activity indicator dots

--- client/src/pages/LoadingPage.jsx
+++ client/src/pages/LoadingPage.jsx
@@ -1,13 +1,42 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, useTheme } from "@mui/material";
 import minutesService from "../services/minutesService";
 import MinutesContext from "../contexts/MinutesContext";
+import logoImage from "../assets/images/logo.png";
+import Image from "../components/Image";
 
 function LoadingPage() {
   const { token } = useParams();
   const [, updateMinutes, updateMetadata] = useContext(MinutesContext);
   const navigate = useNavigate();
+  const theme = useTheme();
+  const [numberOfDots, setNumberOfDots] = useState(0);
+
+  const styles = {
+    container: {
+      backgroundColor: theme.palette.background.main,
+      width: "100vw",
+      height: "100dvh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column",
+    },
+    message: { fontSize: "3rem" },
+    textContainer: {
+      display: "flex",
+    },
+    dots: { fontSize: "3rem", width: 20 },
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setNumberOfDots((previous) => (previous < 3 ? previous + 1 : 0));
+    }, 300);
+
+    return () => clearInterval(intervalId);
+  }, []);
 
   useEffect(() => {
     if (token) {
@@ -24,8 +53,18 @@ function LoadingPage() {
   }, [navigate, token, updateMetadata, updateMinutes]);
 
   return (
-    <Box>
-      <Typography>Loading...</Typography>
+    <Box sx={styles.container}>
+      <Image
+        height="200px"
+        width="200px"
+        src={logoImage}
+        alt="tablebook logo"
+      />
+
+      <Box sx={styles.textContainer}>
+        <Typography sx={styles.message}>Loading</Typography>
+        <Typography sx={styles.dots}>{".".repeat(numberOfDots)}</Typography>
+      </Box>
     </Box>
   );
 }
