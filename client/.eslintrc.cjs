952a0f353279686bb7e468e8d0d4df086e0305df
Tuukka Viitanen <tuukka.viitanen@gmail.com>
2024-03-28 17:04:18 +0200

    Added react refresh eslint plugin back

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -9,6 +9,7 @@ module.exports = {
   ignorePatterns: ['dist'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
   rules: {
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
