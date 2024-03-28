3612a1e5e4cba5f1ee0b96159819b51203bab8b7
Tuukka Viitanen <tuukka.viitanen@gmail.com>
2024-03-28 21:20:45 +0200

    Refactored App.jsx content to EditorPage.jsx
    This way there is room in App.jsx to create routing logic

---
+++ client/src/pages/EditorPage.jsx
@@ -0,0 +1,73 @@
+import { Box, useTheme } from "@mui/material";
+import React, { useState } from "react";
+import TopBar from "../components/TopBar";
+import SideBar from "../components/SideBar";
+import Editor from "../components/Editor";
+import Footer from "../components/Footer";
+import SharePopup from "../components/SharePopup";
+import SignatureModal from "../components/SignatureModal";
+
+function EditorPage() {
+  const theme = useTheme();
+
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const handleModalOpen = () => setModalOpen(true);
+  const handleModalClose = () => setModalOpen(false);
+
+  const styles = {
+    outerContainer: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "space-between",
+      backgroundColor: theme.palette.background.main,
+      height: "100dvh",
+    },
+
+    innerContainer: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      height: "100dvh",
+    },
+
+    editorContainer: {
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+    },
+
+    sidebarImitation: {
+      width: "20vw",
+      minWidth: 280,
+    },
+
+    editorButtonsImitation: {
+      width: "6.66vw",
+      maxWidth: 200,
+    },
+  };
+
+  return (
+    <>
+      <Box sx={styles.outerContainer}>
+        <TopBar />
+        <Box sx={styles.innerContainer}>
+          <SideBar handleModalOpen={handleModalOpen} />
+          <Box sx={styles.editorContainer}>
+            <Editor />
+            {/* mimics editor buttons to center the paper */}
+            <Box sx={styles.editorButtonsImitation} />
+          </Box>
+          {/* mimics sideBar to center the paper */}
+          <Box sx={styles.sidebarImitation} />
+        </Box>
+        <Footer />
+      </Box>
+      <SignatureModal open={isModalOpen} onClose={handleModalClose} />
+      <SharePopup />
+    </>
+  );
+}
+
+export default EditorPage;
