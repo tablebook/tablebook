8e835155dabfabeb6af51f25e29baf4a30b40468
Tuukka Viitanen <tuukka.viitanen@gmail.com>
2024-03-29 03:32:20 +0200

    Fixed lint error

--- client/src/components/Image.jsx
+++ client/src/components/Image.jsx
@@ -2,10 +2,7 @@ import React from "react";
 import { Box } from "@mui/material";
 
 function Image({ src, sx, alt }) {
-  return (
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    <Box component="img" src={src} sx={sx} alt={alt} />
-  );
+  return <Box component="img" src={src} sx={sx} alt={alt} />;
 }
 
 export default Image;
