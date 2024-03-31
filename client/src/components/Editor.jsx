84263b40619573c118d65f66975fbc308b6b8dab
Arto-Laitinen <arto.e.laitinen@tuni.fi>
2024-03-31 13:43:55 +0300

    moved the signature to its own component

--- client/src/components/Editor.jsx
+++ client/src/components/Editor.jsx
@@ -1,10 +1,10 @@
 import React, { useContext } from "react";
-import moment from "moment";
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import Title from "./Title";
 import SideContainer from "./SideContainer";
 import SegmentContainer from "./SegmentContainer";
 import Segment from "./Segment";
+import Signature from "./Signature";
 import MinutesContext from "../contexts/MinutesContext";
 
 function Editor() {
@@ -33,46 +33,6 @@ function Editor() {
       justifyContent: "end",
       width: 1000,
     },
-
-    dateAndSignatureContainer: {
-      display: "flex",
-      flexDirection: "row",
-      pb: 2,
-    },
-
-    signatureContainer: {
-      display: "flex",
-      flexDirection: "column",
-      flex: 1,
-      justifyContent: "flex-end",
-      pl: 2,
-    },
-
-    signatureImage: {
-      maxHeight: 40,
-      maxWidth: 150,
-      objectFit: "contain",
-      alignSelf: "flex-start",
-    },
-
-    signatureAndDateLine: {
-      borderTop: `2px solid`,
-      color: state.minutes.colors.primary,
-      width: 150,
-    },
-
-    dateContainer: {
-      display: "flex",
-      flexDirection: "column",
-      flex: 1,
-      justifyContent: "flex-end",
-      alignItems: "flex-end",
-      pr: 2,
-    },
-
-    signatureAndDateText: {
-      color: state.minutes.colors.primary,
-    },
   };
 
   return (
@@ -92,50 +52,7 @@ function Editor() {
       <Box sx={styles.bottomContainer}>
         <SideContainer />
         <Box sx={styles.bottomContentContainer}>
-          <Box sx={styles.dateAndSignatureContainer}>
-            <Box sx={styles.signatureContainer}>
-              {state.minutes.signatures.length > 0 &&
-                state.minutes.signatures[0].image && (
-                  <Box
-                    sx={styles.signatureImage}
-                    component="img"
-                    src={state.minutes.signatures[0].image}
-                    alt="Signature"
-                  />
-                )}
-              <Box sx={styles.signatureAndDateLine} />
-              {state.minutes.signatures.length > 0 &&
-                state.minutes.signatures[0].signer && (
-                  <Typography variant="h5" sx={styles.signatureAndDateText}>
-                    {state.minutes.signatures[0].signer}
-                  </Typography>
-                )}
-              <Typography variant="h5" sx={styles.signatureAndDateText}>
-                Signature
-              </Typography>
-            </Box>
-            <Box sx={styles.dateContainer}>
-              {state.minutes.signatures.length > 0 &&
-                state.minutes.signatures[0].timestamp && (
-                  <Box sx={styles.dateContainer}>
-                    <Typography variant="h5" sx={styles.signatureAndDateText}>
-                      {moment
-                        .utc(state.minutes.signatures[0].timestamp)
-                        .format("YYYY-MM-DD")}
-                    </Typography>
-                    <Typography variant="h5" sx={styles.signatureAndDateText}>
-                      {moment
-                        .utc(state.minutes.signatures[0].timestamp)
-                        .format("HH:mm")}
-                    </Typography>
-                  </Box>
-                )}
-              <Box sx={styles.signatureAndDateLine} />
-              <Typography variant="h5" sx={styles.signatureAndDateText}>
-                Date
-              </Typography>
-            </Box>
-          </Box>
+          <Signature />
         </Box>
       </Box>
     </Box>
---
+++ client/src/components/Signature.jsx
@@ -0,0 +1,104 @@
+import React, { useContext } from "react";
+import moment from "moment";
+import { Box, Typography } from "@mui/material";
+import MinutesContext from "../contexts/MinutesContext";
+
+function Signature() {
+  const [state] = useContext(MinutesContext);
+  const styles = {
+    dateAndSignatureContainer: {
+      display: "flex",
+      flexDirection: "row",
+      pb: 2,
+    },
+
+    signatureContainer: {
+      display: "flex",
+      flexDirection: "column",
+      flex: 1,
+      justifyContent: "flex-end",
+      pl: 2,
+    },
+
+    signatureImage: {
+      maxHeight: 40,
+      maxWidth: 150,
+      objectFit: "contain",
+      alignSelf: "flex-start",
+    },
+
+    signatureAndDateLine: {
+      borderTop: `2px solid`,
+      color: state.minutes.colors.primary,
+      width: 150,
+    },
+
+    dateContainer: {
+      display: "flex",
+      flexDirection: "column",
+      flex: 1,
+      justifyContent: "flex-end",
+      alignItems: "flex-end",
+      pr: 2,
+    },
+
+    signatureAndDateText: {
+      color: state.minutes.colors.primary,
+    },
+  };
+
+  return (
+    <Box sx={styles.dateAndSignatureContainer}>
+      <Box sx={styles.signatureContainer}>
+        {state.minutes.signatures.length > 0 &&
+          state.minutes.signatures[0].image && (
+            <Box
+              sx={styles.signatureImage}
+              component="img"
+              src={state.minutes.signatures[0].image}
+              alt="Signature"
+            />
+          )}
+
+        <Box sx={styles.signatureAndDateLine} />
+
+        {state.minutes.signatures.length > 0 &&
+          state.minutes.signatures[0].signer && (
+            <Typography variant="h5" sx={styles.signatureAndDateText}>
+              {state.minutes.signatures[0].signer}
+            </Typography>
+          )}
+
+        <Typography variant="h5" sx={styles.signatureAndDateText}>
+          Signature
+        </Typography>
+      </Box>
+
+      <Box sx={styles.dateContainer}>
+        {state.minutes.signatures.length > 0 &&
+          state.minutes.signatures[0].timestamp && (
+            <Box sx={styles.dateContainer}>
+              <Typography variant="h5" sx={styles.signatureAndDateText}>
+                {moment
+                  .utc(state.minutes.signatures[0].timestamp)
+                  .format("YYYY-MM-DD")}
+              </Typography>
+              <Typography variant="h5" sx={styles.signatureAndDateText}>
+                {moment
+                  .utc(state.minutes.signatures[0].timestamp)
+                  .format("HH:mm")}
+              </Typography>
+            </Box>
+          )}
+
+        <Box sx={styles.signatureAndDateLine} />
+
+        <Typography variant="h5" sx={styles.signatureAndDateText}>
+          Date
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
+
+export default Signature;
