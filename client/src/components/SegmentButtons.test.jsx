bd953223109d0032fe490e5152a39d30df7d7d80
Arto-Laitinen <arto.e.laitinen@tuni.fi>
2024-03-31 22:56:51 +0300

    added tests for the delete, moveUp and moveDown buttons
    the tests use an edited version of the mockMinutesContextState just so that there are 3 segments. The rendering also isnt done in beforeAlls except for the delete tests. That is because the up and down button describes needed to have different segment indexes in the tests to test all the functionalities

--- client/src/components/SegmentButtons.test.jsx
+++ client/src/components/SegmentButtons.test.jsx
@@ -1,25 +1,243 @@
 import React from "react";
-import { expect, test, describe, beforeEach } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { expect, test, describe, beforeEach, vi, afterEach } from "vitest";
+import { render, screen, waitFor } from "@testing-library/react";
 import SegmentButtons from "./SegmentButtons";
+import MinutesContext from "../contexts/MinutesContext";
+import { mockMinutesContextState } from "../util/test.helpers";
 
 describe("EditorButtons", () => {
-  beforeEach(() => {
-    render(<SegmentButtons />);
+  const updateMinutesMock = vi.fn();
+
+  const editedMockMinutesContextState = {
+    minutes: {
+      ...mockMinutesContextState.minutes,
+      segments: [
+        {
+          name: "0 title",
+          content: "0 content",
+        },
+        {
+          name: "1 title",
+          content: "1 content",
+        },
+        {
+          name: "2 title",
+          content: "2 content",
+        },
+      ],
+    },
+    metadata: mockMinutesContextState.metadata,
+  };
+
+  afterEach(async () => {
+    vi.restoreAllMocks();
   });
 
-  test("renders the up button", () => {
-    const upButton = screen.getByTestId("upButton");
-    expect(upButton).toBeInTheDocument();
+  describe("Delete button", () => {
+    beforeEach(() => {
+      vi.stubGlobal("confirm", vi.fn());
+
+      render(
+        <MinutesContext.Provider
+          value={[
+            editedMockMinutesContextState,
+            { updateMinutes: updateMinutesMock },
+          ]}
+        >
+          <SegmentButtons segmentIndex={1} />
+        </MinutesContext.Provider>,
+      );
+    });
+
+    afterEach(() => {
+      vi.unstubAllGlobals();
+    });
+
+    test("renders", () => {
+      const deleteButton = screen.getByTestId("deleteButton");
+      expect(deleteButton).toBeInTheDocument();
+    });
+
+    test("on confirm, calls updateMinutes with right values", async () => {
+      window.confirm.mockReturnValueOnce(true);
+
+      const deleteButton = screen.getByTestId("deleteButton");
+
+      deleteButton.click();
+
+      await waitFor(() => {
+        expect(window.confirm).toHaveBeenCalledOnce();
+        expect(updateMinutesMock).toHaveBeenCalledOnce();
+        expect(updateMinutesMock).toHaveBeenCalledWith({
+          segments: [
+            {
+              name: "0 title",
+              content: "0 content",
+            },
+            {
+              name: "2 title",
+              content: "2 content",
+            },
+          ],
+        });
+      });
+    });
+
+    test("on cancel, doent call updateMinutes", async () => {
+      window.confirm.mockReturnValueOnce(false);
+
+      const deleteButton = screen.getByTestId("deleteButton");
+
+      deleteButton.click();
+
+      await waitFor(() => {
+        expect(window.confirm).toHaveBeenCalledOnce();
+        expect(updateMinutesMock).not.toHaveBeenCalled();
+      });
+    });
   });
 
-  test("renders the delete button", () => {
-    const upButton = screen.getByTestId("deleteButton");
-    expect(upButton).toBeInTheDocument();
+  describe("Up button", () => {
+    test("renders", () => {
+      render(
+        <MinutesContext.Provider
+          value={[
+            editedMockMinutesContextState,
+            { updateMinutes: updateMinutesMock },
+          ]}
+        >
+          <SegmentButtons segmentIndex={1} />
+        </MinutesContext.Provider>,
+      );
+
+      const upButton = screen.getByTestId("upButton");
+      expect(upButton).toBeInTheDocument();
+    });
+
+    test("calls the updateMinutes with right values", () => {
+      render(
+        <MinutesContext.Provider
+          value={[
+            editedMockMinutesContextState,
+            { updateMinutes: updateMinutesMock },
+          ]}
+        >
+          <SegmentButtons segmentIndex={1} />
+        </MinutesContext.Provider>,
+      );
+
+      const upButton = screen.getByTestId("upButton");
+
+      upButton.click();
+
+      expect(updateMinutesMock).toHaveBeenCalledOnce();
+      expect(updateMinutesMock).toHaveBeenCalledWith({
+        segments: [
+          {
+            name: "1 title",
+            content: "1 content",
+          },
+          {
+            name: "0 title",
+            content: "0 content",
+          },
+          {
+            name: "2 title",
+            content: "2 content",
+          },
+        ],
+      });
+    });
+
+    test("doesnt call updateMinutes when first segments button clicked", () => {
+      render(
+        <MinutesContext.Provider
+          value={[
+            editedMockMinutesContextState,
+            { updateMinutes: updateMinutesMock },
+          ]}
+        >
+          <SegmentButtons segmentIndex={0} />
+        </MinutesContext.Provider>,
+      );
+
+      const upButton = screen.getByTestId("upButton");
+
+      upButton.click();
+
+      expect(updateMinutesMock).not.toHaveBeenCalled();
+    });
   });
 
-  test("renders the down button", () => {
-    const upButton = screen.getByTestId("downButton");
-    expect(upButton).toBeInTheDocument();
+  describe("Down button", () => {
+    test("renders", () => {
+      render(
+        <MinutesContext.Provider
+          value={[
+            editedMockMinutesContextState,
+            { updateMinutes: updateMinutesMock },
+          ]}
+        >
+          <SegmentButtons segmentIndex={1} />
+        </MinutesContext.Provider>,
+      );
+
+      const downButton = screen.getByTestId("downButton");
+      expect(downButton).toBeInTheDocument();
+    });
+
+    test("calls the updateMinutes with right values", () => {
+      render(
+        <MinutesContext.Provider
+          value={[
+            editedMockMinutesContextState,
+            { updateMinutes: updateMinutesMock },
+          ]}
+        >
+          <SegmentButtons segmentIndex={1} />
+        </MinutesContext.Provider>,
+      );
+
+      const downButton = screen.getByTestId("downButton");
+
+      downButton.click();
+
+      expect(updateMinutesMock).toHaveBeenCalledOnce();
+      expect(updateMinutesMock).toHaveBeenCalledWith({
+        segments: [
+          {
+            name: "0 title",
+            content: "0 content",
+          },
+          {
+            name: "2 title",
+            content: "2 content",
+          },
+          {
+            name: "1 title",
+            content: "1 content",
+          },
+        ],
+      });
+    });
+
+    test("doesnt call updateMinutes when last segments button clicked", () => {
+      render(
+        <MinutesContext.Provider
+          value={[
+            editedMockMinutesContextState,
+            { updateMinutes: updateMinutesMock },
+          ]}
+        >
+          <SegmentButtons segmentIndex={2} />
+        </MinutesContext.Provider>,
+      );
+
+      const downButton = screen.getByTestId("downButton");
+
+      downButton.click();
+
+      expect(updateMinutesMock).not.toHaveBeenCalled();
+    });
   });
 });
