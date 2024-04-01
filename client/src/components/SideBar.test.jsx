2e97a8e956a9359553576b09cb6110f1894091e8
Arto-Laitinen <arto.e.laitinen@tuni.fi>
2024-04-01 18:27:28 +0300

    removed all the color and language tests from the sidebar tests

--- client/src/components/SideBar.test.jsx
+++ client/src/components/SideBar.test.jsx
@@ -32,61 +32,6 @@ describe("SideBar", () => {
     );
   });
 
-  test("color picker colors are equal to editor colors", () => {
-    const colorPickerBoxes = screen.getAllByTestId("color-picker-box");
-    const backgroundColor = window
-      .getComputedStyle(colorPickerBoxes[0])
-      .getPropertyValue("background-color");
-    const rgbMatch = backgroundColor.match(/rgb\((\d+), (\d+), (\d+)\)/);
-    if (rgbMatch) {
-      const red = parseInt(rgbMatch[1], 10)
-        .toString(16)
-        .toUpperCase()
-        .padStart(2, "0");
-      const green = parseInt(rgbMatch[2], 10)
-        .toString(16)
-        .toUpperCase()
-        .padStart(2, "0");
-      const blue = parseInt(rgbMatch[3], 10)
-        .toString(16)
-        .toUpperCase()
-        .padStart(2, "0");
-      const actualHex = `#${red}${green}${blue}`;
-      expect(actualHex).toBe(mockMinutesContextState.minutes.colors.primary);
-    }
-  });
-
-  test("renders the primary color element", () => {
-    const primaryColorBox = screen.getByText("Text color");
-    expect(primaryColorBox).toBeDefined();
-  });
-
-  test("renders the secondary color element", () => {
-    const secondaryColorBox = screen.getByText("Background color");
-    expect(secondaryColorBox).toBeDefined();
-  });
-
-  test("renders the restore defaults button", () => {
-    const restoreButton = screen.getByText("Restore defaults", {
-      selector: "button",
-    });
-    expect(restoreButton).toBeDefined();
-  });
-
-  test("render the flag trigger button", () => {
-    const flagTrigger = screen.getByTestId("flagTrigger");
-    expect(flagTrigger).toBeInTheDocument();
-  });
-
-  test("open flag picker on click of flag tigger button and close on reclick", () => {
-    const flagTrigger = screen.getByTestId("flagTrigger");
-    fireEvent.click(flagTrigger);
-    const flagPicker = screen.getByTestId("flagPicker");
-    expect(flagPicker).toBeInTheDocument();
-    fireEvent.click(flagTrigger);
-    expect(flagPicker).not.toBeInTheDocument();
-  });
-
   describe("add a field button", () => {
     test("renders", () => {
       const addAFieldButton = screen.getByText("Add a field", {
