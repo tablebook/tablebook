6727973f09a8d5a3cb8cfc4f8c435b0ae8a137ec
Tuukka Viitanen <tuukka.viitanen@gmail.com>
2024-04-04 13:35:48 +0300

    Added error handling to reload button

--- client/src/components/TopBar.jsx
+++ client/src/components/TopBar.jsx
@@ -128,26 +128,32 @@ function TopBar() {
   };
 
   const handleReloadClicked = async () => {
-    const minutesResponse = await minutesService.getMinutesByToken(
-      minutesState.metadata.writeToken ?? minutesState.metadata.readToken,
-    );
+    try {
+      const minutesResponse = await minutesService.getMinutesByToken(
+        minutesState.metadata.writeToken ?? minutesState.metadata.readToken,
+      );
 
-    if (
-      !_.isEqual(minutesState.minutes, minutesResponse.data) && // If state differs from incoming minutes
-      !window.confirm(
-        "There are changes in the incoming minutes. Are you sure you want to overwrite the current minutes?",
-      ) // If user cancels
-    ) {
-      return;
+      if (
+        !_.isEqual(minutesState.minutes, minutesResponse.data) && // If state differs from incoming minutes
+        !window.confirm(
+          "There are changes in the incoming minutes. Are you sure you want to overwrite the current minutes?",
+        ) // If user cancels
+      ) {
+        return;
+      }
+
+      updateMinutes(minutesResponse.data);
+
+      updateMetadata({
+        writeAccess: minutesResponse.writeAccess,
+        readToken: minutesResponse.readToken,
+        writeToken: minutesResponse.writeToken,
+      });
+
+      alert("Successfully loaded minutes");
+    } catch (error) {
+      alert("Reloading minutes failed");
     }
-
-    updateMinutes(minutesResponse.data);
-
-    updateMetadata({
-      writeAccess: minutesResponse.writeAccess,
-      readToken: minutesResponse.readToken,
-      writeToken: minutesResponse.writeToken,
-    });
   };
 
   return (
