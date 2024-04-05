0347646458e7a58dde8a5f036ee99aecaddda3e6
Arto-Laitinen <arto.e.laitinen@tuni.fi>
2024-04-05 17:37:36 +0300

    added the editorContext to localstorage
    the editorContext is now saved to localstorage on every update. On page update it checks if there is editorcontext in the localstorage and if there is it uses it or if there isnt it uses the initial state

--- client/src/contexts/EditorContext.jsx
+++ client/src/contexts/EditorContext.jsx
@@ -2,18 +2,29 @@ import React, { createContext, useMemo, useState } from "react";
 
 const EditorContext = createContext();
 
+const initialState = {
+  language: "EN",
+  isSignatureModalOpen: false,
+  sharePopupAnchorElement: null,
+};
+
 export function EditorContextProvider({ children }) {
-  const [editor, setEditor] = useState({
-    language: "EN",
-    isSignatureModalOpen: false,
-    sharePopupAnchorElement: null,
+  const [editor, setEditor] = useState(() => {
+    const storedEditorContext = localStorage.getItem("EditorContext");
+    return storedEditorContext !== null
+      ? JSON.parse(storedEditorContext)
+      : initialState;
   });
 
   const updateEditor = (newEditorData) => {
-    setEditor((prevEditor) => ({
-      ...prevEditor,
-      ...newEditorData,
-    }));
+    setEditor((prevEditor) => {
+      const newState = {
+        ...prevEditor,
+        ...newEditorData,
+      };
+      localStorage.setItem("EditorContext", JSON.stringify(newState));
+      return newState;
+    });
   };
 
   const contextValues = useMemo(() => [editor, updateEditor], [editor]);
