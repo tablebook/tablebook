c6806379675e5d6aac7aaf1fdcb462cd26d9c837
TyostoKarry <samuritala@hotmail.com>
2024-04-08 12:51:27 +0300

    Added signature color update to MinutesContext
    - Updates signatures with a new ones that has correct colors
    - Signature update is debounced with one second
    - Replaced self made debounce implementation with use-debounce dependency

--- client/src/components/ColorPickerContainer.jsx
+++ client/src/components/ColorPickerContainer.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { Box, Button, useTheme, Typography } from "@mui/material";
 import { useTranslation } from "react-i18next";
+import { useDebouncedCallback } from "use-debounce";
 import ColorPicker from "./ColorPicker";
 import MinutesContext from "../contexts/MinutesContext";
 
@@ -8,6 +9,22 @@ function ColorPickerContainer() {
   const theme = useTheme();
   const [minutesState, { updateMinutes }] = useContext(MinutesContext);
   const { t } = useTranslation();
+  const updateSignatureDebounced = useDebouncedCallback(
+    // function
+    (value) => {
+      updateMinutes(value);
+    },
+    // delay in ms
+    1000,
+  );
+  const updateColorDebounced = useDebouncedCallback(
+    // function
+    (value) => {
+      updateMinutes(value);
+    },
+    // delay in ms
+    2,
+  );
 
   const styles = {
     colorPickerContainer: {
@@ -49,26 +66,79 @@ function ColorPickerContainer() {
     },
   };
 
-  // debouncer is to delay state updates, because rapid
-  // state updates on color picker throws error
-  const debounce = (func, wait) => {
-    let timeoutId = null;
-    return (...args) => {
-      clearTimeout(timeoutId);
-      timeoutId = setTimeout(() => {
-        func(...args);
-      }, wait);
-    };
+  const hexToRgb = (hex) => {
+    const hexIsShort = hex.length === 4;
+    const rgbRed = parseInt(hexIsShort ? hex[1] + hex[1] : hex[1] + hex[2], 16);
+    const rgbGreen = parseInt(
+      hexIsShort ? hex[2] + hex[2] : hex[3] + hex[4],
+      16,
+    );
+    const rgbBlue = parseInt(
+      hexIsShort ? hex[3] + hex[3] : hex[5] + hex[6],
+      16,
+    );
+    return [rgbRed, rgbGreen, rgbBlue];
   };
 
-  const updateColor = debounce((type, color) => {
-    updateMinutes({
+  const changeSignatureColorsToContext = (color) => {
+    minutesState.minutes.signatures.forEach((signature) => {
+      if (signature.image) {
+        const newSignature = new Image();
+        newSignature.src = minutesState.minutes.signatures[0].image;
+        newSignature.onload = () => {
+          const canvas = document.createElement("canvas");
+          const canvasContext = canvas.getContext("2d");
+          canvas.width = newSignature.width;
+          canvas.height = newSignature.height;
+
+          canvasContext.drawImage(newSignature, 0, 0);
+
+          const canvasData = canvasContext.getImageData(
+            0,
+            0,
+            canvas.width,
+            canvas.height,
+          );
+          const { data } = canvasData;
+
+          const [rgbRed, rgbGreen, rgbBlue] = hexToRgb(color);
+
+          for (let i = 0; i < data.length; i += 4) {
+            // Only change non-transparent pixels
+            if (data[i + 3] > 0) {
+              data[i] = rgbRed;
+              data[i + 1] = rgbGreen;
+              data[i + 2] = rgbBlue;
+            }
+          }
+
+          canvasContext.putImageData(canvasData, 0, 0);
+
+          updateSignatureDebounced({
+            signatures: [
+              {
+                ...signature,
+                image: canvas.toDataURL(),
+              },
+            ],
+          });
+        };
+      }
+    });
+  };
+
+  const updateColor = (type, color) => {
+    updateColorDebounced({
       colors: {
         ...minutesState.minutes.colors,
         [type]: color,
       },
     });
-  }, 2);
+
+    if (type === "primary") {
+      changeSignatureColorsToContext(color);
+    }
+  };
 
   const defaultColors = {
     primary: "#000000",
