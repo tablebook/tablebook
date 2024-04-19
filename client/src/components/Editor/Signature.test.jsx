2202dcd3cead82fe376953ad8cc5c207341eaca1
Arto-Laitinen <arto.e.laitinen@tuni.fi>
2024-04-19 12:50:58 +0300

    updated signature tests to match the updates
    signature tests now check that everything renders with signature that has content and the signature and date lines still render with an empty signature

--- client/src/components/Editor/Signature.test.jsx
+++ client/src/components/Editor/Signature.test.jsx
@@ -8,37 +8,59 @@ import MinutesContext from "../../contexts/MinutesContext";
 import { mockMinutesContextState } from "../../util/test.helpers";
 
 describe("Signature", () => {
-  beforeEach(() => {
+  const renderWith = (signatureIndex) => {
     render(
       <MinutesContext.Provider value={[mockMinutesContextState, {}]}>
         <I18nextProvider i18n={i18n}>
-          <Signature />
+          <Signature signatureIndex={signatureIndex} />
         </I18nextProvider>
       </MinutesContext.Provider>,
     );
+  };
+
+  describe("with signature that has content", () => {
+    beforeEach(() => {
+      renderWith(0);
+    });
+
+    test("renders signature and date sections", () => {
+      const signatureSection = screen.getByText("Signature");
+      const dateSection = screen.getByText("Date");
+      expect(signatureSection).toBeInTheDocument();
+      expect(dateSection).toBeInTheDocument();
+    });
+
+    test("renders signature image with correct source", () => {
+      const signatureImage = screen.getByAltText("Signature");
+      expect(signatureImage).toBeInTheDocument();
+      expect(signatureImage.src).toEqual(
+        "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAQAAAAB7CAYAAACb4F7QAAAAAXNSR0I",
+      );
+    });
+
+    test("renders signer name clarification", () => {
+      const nameClarification = screen.getByText("Test User");
+      expect(nameClarification).toBeInTheDocument();
+    });
+
+    test("renders date timestamp", () => {
+      const dateString = screen.getByText("2024-03-30");
+      const timeString = screen.getByText("00:00 UTC");
+      expect(dateString).toBeInTheDocument();
+      expect(timeString).toBeInTheDocument();
+    });
   });
 
-  test("renders signature and date sections", () => {
-    const signatureSection = screen.getByText("Signature");
-    const dateSection = screen.getByText("Date");
-    expect(signatureSection).toBeInTheDocument();
-    expect(dateSection).toBeInTheDocument();
-  });
+  describe("with empty signature", () => {
+    beforeEach(() => {
+      renderWith(1);
+    });
 
-  test("renders signature image with correct source", () => {
-    const signatureImage = screen.getByAltText("Signature");
-    expect(signatureImage).toBeInTheDocument();
-  });
-
-  test("renders signer name clarification", () => {
-    const nameClarification = screen.getByText("Test User");
-    expect(nameClarification).toBeInTheDocument();
-  });
-
-  test("renders date timestamp", () => {
-    const dateString = screen.getByText("2024-03-30");
-    const timeString = screen.getByText("00:00 UTC");
-    expect(dateString).toBeInTheDocument();
-    expect(timeString).toBeInTheDocument();
+    test("renders signature and date sections", () => {
+      const signatureSection = screen.getByText("Signature");
+      const dateSection = screen.getByText("Date");
+      expect(signatureSection).toBeInTheDocument();
+      expect(dateSection).toBeInTheDocument();
+    });
   });
 });
