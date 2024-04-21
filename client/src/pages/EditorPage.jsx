8922dd9a5e74793d822f3d02f91c1de14217a1f9
Tuukka Viitanen <tuukka.viitanen@gmail.com>
2024-04-21 14:54:08 +0300

    Moved editor component right spacing styles to styles

--- client/src/pages/EditorPage.jsx
+++ client/src/pages/EditorPage.jsx
@@ -43,6 +43,7 @@ function EditorPage() {
       display: "flex",
       flexDirection: "row",
     },
+    editorRightSpacing: { width: "24vw", minWidth: "5vw" },
   };
 
   useEffect(() => {
@@ -75,7 +76,7 @@ function EditorPage() {
         <SideBar />
         <Box sx={styles.scrollable}>
           <Editor />
-          <Box sx={{ width: "24vw", minWidth: "5vw" }} />
+          <Box sx={styles.editorRightSpacing} />
         </Box>
       </Box>
       <Box sx={styles.footer}>
