024a19f3622217da5c7a220f62aa43448c33a726
TyostoKarry <samuritala@hotmail.com>
2024-04-21 15:07:10 +0300

    Moved DownloadPDFButton to its own component
    - Created new component DownloadPDFButton
    - Takes variant, download ready buttons color and sx style as props

---
+++ client/src/components/DownloadPDFButton.jsx
@@ -0,0 +1,94 @@
+import React, { useContext, useState, useEffect } from "react";
+import { Button } from "@mui/material";
+import { usePDF } from "@react-pdf/renderer";
+import { toast } from "react-toastify";
+import { useTranslation } from "react-i18next";
+import moment from "moment";
+import MinutesContext from "../contexts/MinutesContext";
+import PDFDocument from "./PDFDocument";
+import prepareMinutesForPDF from "../util/prepareMinutesForPDF";
+
+function DownloadPDFButton({ variant, color, sx }) {
+  const { t } = useTranslation();
+  const [minutesState] = useContext(MinutesContext);
+  const [PDFInstance, updatePDFInstance] = usePDF({
+    document: (
+      <PDFDocument pdfReadyMinutes={prepareMinutesForPDF(minutesState)} />
+    ),
+  });
+  const [triggerPDFDownload, setTriggerPDFDownload] = useState(false);
+
+  useEffect(() => {
+    if (PDFInstance.error)
+      toast.error(t("errorInPDFGeneration"), {
+        toastId: "PDF-Document-Error",
+      });
+  }, [PDFInstance.error, t]);
+
+  const handlePDFDownload = () => {
+    updatePDFInstance(
+      <PDFDocument pdfReadyMinutes={prepareMinutesForPDF(minutesState)} />,
+    );
+    setTriggerPDFDownload(true);
+  };
+
+  // Downloads PDF only after the PDFInstance is updated
+  useEffect(() => {
+    if (
+      triggerPDFDownload &&
+      PDFInstance.url &&
+      !PDFInstance.loading &&
+      !PDFInstance.error
+    ) {
+      const link = document.createElement("a");
+      link.href = PDFInstance.url;
+      link.download = `${minutesState.minutes.name.split(" ")[0]}_${moment().format("YYYY-MM-DD")}.pdf`;
+      document.body.appendChild(link);
+      link.click();
+      link.remove();
+
+      setTriggerPDFDownload(false);
+    }
+  }, [
+    PDFInstance.url,
+    PDFInstance.loading,
+    PDFInstance.error,
+    triggerPDFDownload,
+    minutesState.minutes.name,
+  ]);
+
+  return (
+    // eslint-disable-next-line no-nested-ternary
+    PDFInstance.error ? (
+      <Button
+        data-testid="download-pdf-button-error"
+        variant="contained"
+        disabled
+        sx={sx}
+      >
+        {t("errorInPDFGeneration")}
+      </Button>
+    ) : PDFInstance.loading ? (
+      <Button
+        data-testid="download-pdf-button"
+        variant="contained"
+        disabled
+        sx={sx}
+      >
+        {t("loadingDocument")}
+      </Button>
+    ) : (
+      <Button
+        data-testid="download-pdf-button"
+        variant={variant}
+        color={color}
+        onClick={handlePDFDownload}
+        sx={sx}
+      >
+        {t("downloadPDF")}
+      </Button>
+    )
+  );
+}
+
+export default DownloadPDFButton;
